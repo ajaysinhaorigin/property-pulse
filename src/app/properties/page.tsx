4f106dd0d7e1611549921e7553157ebce3d9371f
Ajay Sinha <ajaysinhaa09@gmail.com>
2024-10-07 22:07:43 +0530

    fetched properties

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import properties from '@/Shared/Constants/properties.json'
-import { PropertyCard } from '@/Shared'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react"
 
-const Properties = () => {
+import { PropertyCard } from "@/Shared"
+
+async function getProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`)
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch properties")
+    }
+    return res.json()
+  } catch (error) {
+    console.log("erorr", error)
+  }
+}
+
+const Properties = async () => {
+  const properties = await getProperties()
+  console.log('properties', properties.properties.length)
+
+    // properties.properties.sort((a: any, b:any) => new Date(b.createdAt) - new Date(a.createdAt))
+
   return (
     <section className="px-4 py-6">
-    <div className="container-xl lg:container m-auto px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {
-            properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))
-          }
+          {properties.properties.map((property: any) => (
+            <PropertyCard key={property._id} property={property} />
+          ))}
         </div>
       </div>
-      </section>
+    </section>
   )
 }
 
