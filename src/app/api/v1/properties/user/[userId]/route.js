dd76e0f559db7d3b59c91107678e41c984e8b596
Ajay Sinha <ajaysinhaa09@gmail.com>
2024-11-03 15:29:17 +0530

    file name changes

--- src/app/api/v1/properties/user/[userId]/route.js
+++ src/app/api/v1/properties/user/[userId]/route.js
@@ -1,5 +1,5 @@
 import connectDB from "@/Config/dbConnection"
-import Property from "@/Models/Propertry"
+import Property from "@/Models/Property.js"
 
 // GET /api/properties/user/:userId
 export const GET = async (request, { params }) => {
